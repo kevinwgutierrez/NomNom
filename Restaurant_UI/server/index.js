4daef86b8b0b4f3eb4c353e4de2730dd59412277
Bryce Balbon <bbalbon@gmail.com>
2020-02-13 13:56:25 -0800

    Change port on restaurant side

--- Restaurant_UI/server/index.js
+++ Restaurant_UI/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const parser = require('body-parser');
-const PORT = 8080;
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(parser.urlencoded({extended: true}));
