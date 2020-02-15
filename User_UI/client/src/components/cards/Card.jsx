a06f168e2c219f57979b778fe9dd9350aef7361d
Zack Singh <zacharysingh@gmail.com>
2020-02-15 12:22:14 -0800

    Setup initial component file structure

---
+++ User_UI/client/src/components/cards/Card.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+    <div>
+      Card List
+    </div>
+    )
+  }
+}
+
+export default Card;
---
+++ User_UI/client/src/components/cards/CardList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return (
+    <div>
+      Card List
+    </div>
+    )
+  }
+}
+
+export default CardList;
