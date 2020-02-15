58f58370a1dd55c2bcf3485797c44d1f91b586f6
Alex Kim <alexkim612@gmail.com>
2020-02-15 15:09:26 -0800

    refactor to add price to transaction history

--- Database/seed.js
+++ Database/seed.js
@@ -14,7 +14,7 @@ const restaurant1 = new RestaurantDB({
   resAddress: '115 Montgomery Street, San Francisco CA 94105',
   resLocation: [37.790343, -122.402202],
   transHist: [
-    { meal: 'Chicken Sandwhich', customer: 'Kevin', status: 'pickedUp', quantity: 1 },
+    { meal: 'Chicken Sandwhich', customer: 'Kevin', status: 'pickedUp', price: 4.50, quantity: 1 },
   ],
   resMeals: [
     { name: 'Chicken Sandwhich', active: true, price: 4.50, quantity: 5, picture: 'https://www.omahasteaks.com/gifs/990x594/rb035.jpg' },
@@ -32,7 +32,7 @@ const restaurant2 = new RestaurantDB({
   resAddress: '1029 Grant Avenue, San Francisco CA 94133',
   resLocation: [37.796389, -122.406721],
   transHist: [
-    { meal: 'Pork Bun', customer: 'Adrian', status: 'pickedUp', quantity: 4 },
+    { meal: 'Pork Bun', customer: 'Adrian', status: 'pickedUp', price: 10, quantity: 4 },
   ],
   resMeals: [
     { name: 'Pork Bun', active: true, price: 2.50, quantity: 8, picture: 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/08/05/pork-buns-ck-1734314-x.jpg?itok=n7Pis2lZ' },
@@ -50,7 +50,7 @@ const restaurant3 = new RestaurantDB({
   resAddress: '201 S 3rd Street Suite 100, San Francisco, CA 94103',
   resLocation: [37.784736, -122.400133],
   transHist: [
-    { meal: '12oz Ribeye', customer: 'Bryce', status: 'pickedUp', quantity: 2 },
+    { meal: '12oz Ribeye', customer: 'Bryce', status: 'pickedUp', price: 24, quantity: 2 },
   ],
   resMeals: [
     { name: '12oz Ribeye', active: true, price: 12, quantity: 10, picture: 'https://www.calvettimeats.com/wp-content/uploads/2016/12/Black-Angus-Beef-Ribeye-Steak-12-oz-1024x684.jpg' },
