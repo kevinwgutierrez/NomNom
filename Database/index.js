d358917ec63e52bb4ea14164c566c1da9f2cc86d
Alex Kim <alexkim612@gmail.com>
2020-02-15 12:18:44 -0800

    not working seed

--- Database/index.js
+++ Database/index.js
@@ -17,6 +17,7 @@ const restaurantSchema = mongoose.Schema({
   active: Boolean,
   contact: {email: String, phone: String},
   address: String,
+  location: [Number, Number],
   transHist: [{ meal: String, customer: String, status: String, quantity: Number }],
   meals: [{ name: String, active: Boolean, quantity: Number, picture: String }],
   stats: { totalMeals: Number, revenue: Number, turtles: Number, YUMYUM: Number }
@@ -28,9 +29,9 @@ const userSchema = mongoose.Schema({
   name: String,
   active: Boolean,
   contact: {email: String, phone: String},
-  location: String,
-  delivered: Boolean,
-  stats: [{ totalMealsBought: Number, moneySaved: Number, moneyDonated: Number, turtles: Number, YUMYUM: Number }]
+  address: String,
+  location: [Number, Number],
+  stats: { totalMealsBought: Number, moneySaved: Number, moneyDonated: Number, turtles: Number, YUMYUM: Number }
 });
 
 // Create DRIVER schema
@@ -39,18 +40,30 @@ const driverSchema = mongoose.Schema({
   name: String,
   active: Boolean,
   contact: {email: String, phone: String},
-  location: String,
+  location: [Number, Number],
   mealPickedUp: Boolean,
   mealDroppedOff: Boolean,
   status: String, //pickup or drop off
   // stats: [{ totalMealsBought: Number, moneySaved: Number, moneyDonated: Number, turtles: Number, YUMYUM: Number }]
 });
 
+// Create ORDERS schema
+const orderSchema = mongoose.Schema({
+  id: Number,
+  user: String,
+  userAddress: String,
+  userContact: {email: String, phone: String},
+  restaurant: String,
+  restaurantAddress: String,
+});
 
-const restaurantDB = mongoose.model('restaurant', restaurantSchema);
-const userDB = mongoose.model('user', userSchema);
-const driverDB = mongoose.model('driver', driverSchema);
 
-module.exports = restaurantDB;
-module.exports = userDB;
-module.exports = driverDB
+const RestaurantDB = mongoose.model('restaurant', restaurantSchema);
+const UserDB = mongoose.model('user', userSchema);
+const DriverDB = mongoose.model('driver', driverSchema);
+const OrderDB = mongoose.model('order', orderSchema);
+
+module.exports = RestaurantDB;
+module.exports = UserDB;
+module.exports = DriverDB;
+module.exports = OrderDB;
