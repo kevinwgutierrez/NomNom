58c06f903adff67957e43e106c4732fee5e22889
Zack Singh <zacharysingh@gmail.com>
2020-02-15 12:55:05 -0800

    Render new componenets

--- User_UI/client/src/index.jsx
+++ User_UI/client/src/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("map"));
+ReactDOM.render(<App />, document.getElementById("app"));
