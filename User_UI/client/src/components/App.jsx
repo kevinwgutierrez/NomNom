b7cfc81e930233375d841b3418eb4eff0b5a10a7
Zack Singh <zacharysingh@gmail.com>
2020-02-16 01:40:18 -0800

    Build user app components

--- User_UI/client/src/components/App.jsx
+++ User_UI/client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CardList from './cards/CardList.jsx';
+import CheckoutModal from './cards/CheckoutModal.jsx';
+import Confirmation from './cards/Confirmation.jsx';
 
 import sampleRestaurantData from '../sampleRestaurantData.js';
 
@@ -9,29 +11,95 @@ class App extends Component {
 
     this.state = {
       meals: [],
+      isModalOpen: false,
+      orderConfirmed: false,
+      selectedMeal: {},
+      quantity: 1,
+      restaurants: []
     };
 
+    this.toggleModal = this.toggleModal.bind(this);
+    this.toggleConfirmation = this.toggleConfirmation.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  componentDidMount() {
     this.getMeals();
   }
 
   getMeals() {
     let restaurantData = sampleRestaurantData.restaurantData;
+    let restaurants = this.state.restaurants;
+    let meals = this.state.meals;
     restaurantData.forEach(restaurant => {
       restaurant.resMeals.forEach(meal => {
         if (meal.active) {
           console.log(meal);
-          this.state.meals.push(meal);
+          meals.push(meal);
         }
       });
     });
+    this.setState({
+      meals: meals
+    });
+  }
+
+  toggleModal(meal) {
+    if (!this.state.isModalOpen) {
+      this.setState({
+        isModalOpen: true,
+        selectedMeal: meal,
+      });
+    } else {
+      this.setState({
+        isModalOpen: false
+      });
+    }
+  }
+
+  toggleConfirmation() {
+    if (!this.state.orderConfirmed) {
+      this.setState({
+        orderConfirmed: true
+      });
+    } else {
+      this.setState({
+        orderConfirmed: false
+      });
+    }
+  }
+
+  submitForm(value) {
+    console.log(value);
+    this.setState({
+      quantity: value
+    })
+    this.toggleModal();
+    this.toggleConfirmation();
   }
 
   render() {
-    const { meals } = this.state;
+    const { meals, isModalOpen, orderConfirmed, selectedMeal, quantity } = this.state;
     return (
-    <div>
-      <CardList meals={meals} />
-    </div>
+      <div id="app-container">
+        {orderConfirmed ?
+          <Confirmation selectedMeal={selectedMeal} quantity={quantity} />
+          :
+          <div id="food-container">
+            <CardList meals={meals} toggleModal={this.toggleModal} />
+            {isModalOpen ?
+              <CheckoutModal
+                toggleModal={this.toggleModal}
+                isModalOpen={isModalOpen}
+                toggleConfirmation={this.toggleConfirmation}
+                orderConfirmed={orderConfirmed}
+                submitForm={this.submitForm}
+                >
+              </CheckoutModal>
+              : null}
+          </div>
+        }
+      </div>
     )
   }
 }
--- User_UI/client/src/components/cards/CardList.jsx
+++ User_UI/client/src/components/cards/CardList.jsx
@@ -3,11 +3,13 @@ import Card from './Card.jsx';
 
 function CardList(props) {
   return (
-    <div>
-      <div><h2>Yummy food items</h2></div>
-      <div>
+    <div id="card-list-container">
+      <div id="card-list-header">
+        <h2>Yummy food items</h2>
+      </div>
+      <div id="card-list">
         {props.meals.map(meal => {
-          return <Card meal={meal} key={meal.name} />;
+          return <Card meal={meal} key={meal.name} toggleModal={props.toggleModal} />;
         })}
       </div>
     </div>
