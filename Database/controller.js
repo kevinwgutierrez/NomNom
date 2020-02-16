739c079dc78f3e1e6555e67dbba20d386bf2221e
Alex Kim <alexkim612@gmail.com>
2020-02-15 20:10:00 -0800

    get request created for driver side

---
+++ Database/controller.js
@@ -0,0 +1,35 @@
+const { RestaurantDB } = './index';
+const { UserDB } = './index';
+const { OrderDB } = './index';
+
+module.exports = {
+  user: {
+    get: (req, res) => {
+
+    }, 
+    post: (req, res) => {
+      
+    }
+  },
+
+  driver: {
+    get: (req, res) => {
+      OrderDB.find({}, (err, data) => {
+        if(err) {
+          console.log(err);
+          res.sendStatus(500);
+        } else {
+          res.send(data);
+        }
+      });
+    },
+    patch: (req, res) => {
+      
+    }
+  },
+
+  // business: {
+  //   get: function (req, res) {},
+  //   post: function (req, res) {}
+  // }
+};
--- User_UI/server/router.js
+++ User_UI/server/router.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const controller = require('../../Database/controller');
 
 // Route user GET request from order endpoint
-router.get('/order/user', controller.order.get);
+router.get('/user', controller.user.get);
 
 // Route user GET request from order endpoint
-router.post('/order/user', controller.order.post);
+router.post('/user', controller.user.post);
 
 
 module.exports = router;
