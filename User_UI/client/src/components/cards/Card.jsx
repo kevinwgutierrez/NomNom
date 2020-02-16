190f8375e8f393957f3a312e00cc10538f9d1dc5
Zack Singh <zacharysingh@gmail.com>
2020-02-16 02:25:11 -0800

    Complete data connection minus post

--- User_UI/client/src/components/cards/Card.jsx
+++ User_UI/client/src/components/cards/Card.jsx
@@ -3,14 +3,23 @@ import React, { Component } from "react";
 function Card(props) {
   return (
     <div className="card" >
-      <div className="mealName">
-        <h3>{props.meal.name}</h3>
+      <div className="meal-name">
+        <h2>{props.meal.name}</h2>
+      </div>
+      <div className="restaurant-name">
+        <h3>{props.meal.resName}</h3>
+      </div>
+      <div className="restaurant-address">
+        <h3>{props.meal.resAddress}</h3>
       </div>
       <div className="price">
-        ${props.meal.price}
+        ${props.meal.price.toFixed(2)}
       </div>
-      <div className="quantityRemaining">
+      <div className="quantity-remaining">
         {props.meal.quantity} Remaining
+      </div>
+      <div className="yum-points">
+        YumYum points: {props.meal.YUMYUM}
       </div>
       <button onClick={() => props.toggleModal(props.meal)}>Order</button>
     </div>
--- User_UI/client/src/components/cards/Confirmation.jsx
+++ User_UI/client/src/components/cards/Confirmation.jsx
@@ -10,7 +10,12 @@ function Confirmation(props) {
         <h2>Order details:</h2>
         <div>
           <h3>Meal: {props.selectedMeal.name} x {props.quantity}</h3>
-          <h4>Total: ${props.selectedMeal.price * props.quantity}</h4>
+          <h4>{props.selectedMeal.resName}</h4>
+          <h5>{props.selectedMeal.resAddress}</h5>
+          <h4>Total: ${(props.selectedMeal.price * props.quantity).toFixed(2)}</h4>
+          <h4>YumYum points earned: {props.selectedMeal.YUMYUM * props.quantity}</h4>
+          <h4>Turtles saved: 4</h4>
+          <h4>Seagulls cleaned: 6</h4>
         </div>
       </div>
     </div>
